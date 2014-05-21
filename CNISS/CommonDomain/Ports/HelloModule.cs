82416851f4be28da57c72b5ec1aef053009e3d2a
Dante Castillo <dante.dubon@gmail.com>
2014-05-21 14:13:21 -0600

    Initial commit

---
+++ CNISS/CommonDomain/Ports/HelloModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CNISS
+{
+    public class HelloModule: NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+        }
+
+    }
+}

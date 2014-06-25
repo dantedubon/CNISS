39bac14832cce92d68b2481ad503ffabd3fc7fc6
dantedubon <dante.dubon@gmail.com>
2014-06-24 23:43:38 -0500

    Se agrega modulo de consulta general de Beneficiarios

---
+++ CNISS Tests/Enterprise Test/Entities_Test/Beneficiario_Test/Modules/when_UserGetAllBeneficiarios_Should_ReturnAllBeneficiarios.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CNISS.CommonDomain.Ports.Input.REST.Modules.BeneficiarioModule.Query;
+using CNISS.CommonDomain.Ports.Input.REST.Request.BeneficiarioRequest;
+using CNISS.CommonDomain.Ports.Input.REST.Request.GremioRequest;
+using CNISS.EnterpriseDomain.Domain.Entities;
+using CNISS.EnterpriseDomain.Domain.Repositories;
+using CNISS.EnterpriseDomain.Domain.ValueObjects;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using Machine.Specifications;
+using Moq;
+using Nancy.Testing;
+using It = Machine.Specifications.It;
+
+namespace CNISS_Tests.Enterprise_Test.Entities_Test.Beneficiario_Test.Modules
+{
+    [Subject(typeof (BeneficiarioModuleQuery))]
+    public class when_UserGetAllBeneficiarios_Should_ReturnAllBeneficiarios
+    {
+        static Browser _browser;
+        static BrowserResponse _response;
+        private static IEnumerable<BeneficiarioRequest> _expectedResponse;
+        private static IEnumerable<BeneficiarioRequest> _responseBeneficiarios; 
+        private Establish context = () =>
+        {
+            var beneficiarios =
+                Builder<Beneficiario>.CreateListOfSize(10).All().WithConstructor(() =>
+                    new Beneficiario(Builder<Identidad>.CreateNew().Build(), Builder<Nombre>.CreateNew().Build(),
+                        new DateTime(1984,8,2))
+                    ).All().Build();
+                    
+                   
+            _expectedResponse = getRequests(beneficiarios);
+            var repository = Mock.Of<IBeneficiarioRepositoryReadOnly>();
+
+            Mock.Get(repository).Setup(x => x.getAll()).Returns(beneficiarios);
+
+            _browser = new Browser(
+                x =>
+                {
+                    x.Module<BeneficiarioModuleQuery>();
+                    x.Dependencies(repository);
+                }
+                
+                );
+        };
+
+        private Because of = () =>
+        {
+            _responseBeneficiarios =
+                _browser.GetSecureJson("/enterprise/beneficiarios")
+                    .Body.DeserializeJson<IEnumerable<BeneficiarioRequest>>();
+        };
+
+        It should_return_all_beneficiarios = () => _responseBeneficiarios.ShouldBeEquivalentTo(_expectedResponse);
+
+        private static IEnumerable<BeneficiarioRequest> getRequests(IEnumerable<Beneficiario> beneficiarios)
+        {
+            return beneficiarios.Select(x=> new BeneficiarioRequest()
+            {
+                identidadRequest = new IdentidadRequest() { identidad = x.Id.identidad},
+                fechaNacimiento = x.fechaNacimiento,
+                nombreRequest = new NombreRequest()
+                {
+                    nombres = x.nombre.nombres,
+                    primerApellido = x.nombre.primerApellido,
+                    segundoApellido = x.nombre.segundoApellido
+                },
+                dependienteRequests = new List<DependienteRequest>()
+                
+            }
+                
+                
+                );
+               
+            
+        }
+    }
+}
--- CNISS/CommonDomain/Ports/Input/REST/Modules/BeneficiarioModule/Query/BeneficiarioModuleQuery.cs
+++ CNISS/CommonDomain/Ports/Input/REST/Modules/BeneficiarioModule/Query/BeneficiarioModuleQuery.cs
@@ -1,68 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using CNISS.CommonDomain.Ports.Input.REST.Request.BeneficiarioRequest;
 using CNISS.CommonDomain.Ports.Input.REST.Request.GremioRequest;
+using CNISS.EnterpriseDomain.Domain.Entities;
+using CNISS.EnterpriseDomain.Domain.Repositories;
 using Nancy;
 
 namespace CNISS.CommonDomain.Ports.Input.REST.Modules.BeneficiarioModule.Query
 {
     public class BeneficiarioModuleQuery:NancyModule
     {
-        public BeneficiarioModuleQuery()
+        public BeneficiarioModuleQuery(IBeneficiarioRepositoryReadOnly repository)
         {
             Get["/enterprise/beneficiarios"] = parameters =>
             {
-                var beneficiario = new BeneficiarioRequest();
-                var nombre = new NombreRequest()
-                {
-                    nombres = "Dante Ruben",
-                    primerApellido = "Castillo",
-                    segundoApellido = "Dubón"
-                };
-                var identidad = new IdentidadRequest()
-                {
-                    identidad = "0801198512396"
-                };
+                var beneficiarios = repository.getAll();
+                return Response.AsJson(beneficiarios.Select(getShortBeneficiarioRequest).ToList());
+            };
+        }
 
-                var dependientes = new List<DependienteRequest>()
-                {
-                    new DependienteRequest()
-                    {
-                        edad = 56,
-                        identidadRequest = new IdentidadRequest() {identidad = "0801195732456"},
-                        nombreRequest =
-                            new NombreRequest()
-                            {
-                                nombres = "Lavinia",
-                                primerApellido = "Dubón",
-                                segundoApellido = "Fajardo"
-                            },
-                        parentescoRequest = new ParentescoRequest() {descripcion = "Madre", guid = Guid.NewGuid()}
-                    },
-                    new DependienteRequest()
-                    {
-                        edad = 56,
-                        identidadRequest = new IdentidadRequest() {identidad = "0801195732456"},
-                        nombreRequest =
-                            new NombreRequest()
-                            {
-                                nombres = "Lavinia",
-                                primerApellido = "Dubón",
-                                segundoApellido = "Fajardo"
-                            },
-                        parentescoRequest = new ParentescoRequest() {descripcion = "Madre", guid = Guid.NewGuid()}
-                    }
-                };
-
-                beneficiario.dependienteRequests = dependientes;
-                beneficiario.identidadRequest = identidad;
-                beneficiario.nombreRequest = nombre;
-                beneficiario.fechaNacimiento = new DateTime(1984, 8, 2);
-
-                return Response.AsJson(beneficiario);
-
+        private BeneficiarioRequest getShortBeneficiarioRequest(Beneficiario beneficiario)
+        {
+            return new BeneficiarioRequest()
+            {
+                identidadRequest = new IdentidadRequest() { identidad = beneficiario.Id.identidad},
+                nombreRequest = new NombreRequest() {
+                    nombres = beneficiario.nombre.nombres,
+                    primerApellido = beneficiario.nombre.primerApellido, 
+                    segundoApellido = beneficiario.nombre.segundoApellido
+                },
+                fechaNacimiento = beneficiario.fechaNacimiento,
+                dependienteRequests = new List<DependienteRequest>()
+                
             };
         }
     }

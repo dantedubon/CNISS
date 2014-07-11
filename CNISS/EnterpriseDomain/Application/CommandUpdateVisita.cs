fb56b5d8158f89e3fd6776da9a83957dcb570206
dantedubon <dante.dubon@gmail.com>
2014-07-11 11:07:57 -0500

    Se borran logs

--- CNISS/EnterpriseDomain/Application/CommandUpdateVisita.cs
+++ CNISS/EnterpriseDomain/Application/CommandUpdateVisita.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CNISS.CommonDomain.Application;
+using CNISS.CommonDomain.Domain;
+using CNISS.EnterpriseDomain.Domain.Entities;
+using CNISS.EnterpriseDomain.Domain.Repositories;
 
 namespace CNISS.EnterpriseDomain.Application
 {
-    public class CommandUpdateVisita
+    public class CommandUpdateVisita : CommandUpdateIdentity<Visita>
     {
+        private readonly IVisitaRepositoryReadOnly _repositoryRead;
+
+        public CommandUpdateVisita(IVisitaRepositoryReadOnly repositoryRead,
+            IRepositoryCommands<Visita> repository, Func<IUnitOfWork> unitOfWork) : base(repository, unitOfWork)
+        {
+            _repositoryRead = repositoryRead;
+        }
+
+        public override bool isExecutable(Visita identity)
+        {
+            return base.isExecutable(identity);
+        }
     }
 }

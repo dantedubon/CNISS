33499d4317c8422eba437f1b3d202979383df3a7
dantedubon <dante.dubon@gmail.com>
2014-07-21 09:23:40 -0500

    Se agrega servicio movil de consulta de motivos de despido

--- CNISS/CommonDomain/Ports/Input/REST/Modules/MotivoDespidoModule/Query/MotivoDespidoModuleQuery.cs
+++ CNISS/CommonDomain/Ports/Input/REST/Modules/MotivoDespidoModule/Query/MotivoDespidoModuleQuery.cs
@@ -1,5 +1,6 @@
 using CNISS.EnterpriseDomain.Domain.Repositories;
 using Nancy;
+using Nancy.Security;
 
 namespace CNISS.CommonDomain.Ports.Input.REST.Modules.MotivoDespidoModule.Query
 {
@@ -8,6 +9,12 @@ namespace CNISS.CommonDomain.Ports.Input.REST.Modules.MotivoDespidoModule.Query
         public MotivoDespidoModuleQuery(IMotivoDespidoRepositoryReadOnly readOnlyRepository)
         {
             Get["/enterprise/motivoDespido"] = parameters => Response.AsJson(readOnlyRepository.getAll());
+
+            Get["/movil/motivosDespido"] = parameters =>
+            {
+                this.RequiresClaims(new[] {"movil"});
+                return Response.AsJson(readOnlyRepository.getAll());
+            };
         }
     }
 }

a8a2bb6898a686155a08bb6bc86f1408038bacf8
dantedubon <dante.dubon@gmail.com>
2014-07-30 11:57:13 -0500

    Fixed last unit test from insert ficha supervision

--- CNISS Tests/Enterprise Test/Entities_Test/Empleo_Test/Module/when_UserPostFichaDeEmpleoFotografiaBeneficiarioNotExists_Should_ReturnError.cs
+++ CNISS Tests/Enterprise Test/Entities_Test/Empleo_Test/Module/when_UserPostFichaDeEmpleoFotografiaBeneficiarioNotExists_Should_ReturnError.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using CNISS.AutenticationDomain.Domain.Entities;
+using CNISS.AutenticationDomain.Domain.Services;
 using CNISS.AutenticationDomain.Domain.ValueObjects;
 using CNISS.CommonDomain.Application;
+using CNISS.CommonDomain.Ports;
 using CNISS.CommonDomain.Ports.Input.REST;
 using CNISS.CommonDomain.Ports.Input.REST.Modules.EmpleoModule.Commands;
+using CNISS.CommonDomain.Ports.Input.REST.Request;
 using CNISS.CommonDomain.Ports.Input.REST.Request.AuditoriaRequest;
 using CNISS.CommonDomain.Ports.Input.REST.Request.BeneficiarioRequest;
 using CNISS.CommonDomain.Ports.Input.REST.Request.EmpleoRequest;
@@ -20,6 +23,7 @@ using CNISS.EnterpriseDomain.Domain.ValueObjects;
 using Machine.Specifications;
 using Moq;
 using Nancy;
+using Nancy.Authentication.Token;
 using Nancy.Testing;
 using Should;
 using It = Machine.Specifications.It;
@@ -32,12 +36,25 @@ namespace CNISS_Tests.Enterprise_Test.Entities_Test.Empleo_Test.Module
         private static Browser _browser;
         private static BrowserResponse _browserResponse;
         private static FichaSupervisionEmpleoRequest _fichaSupervisionEmpleoRequest;
-
+        private static MovilRequest _dummyMovilRequest;
 
         private Establish context = () =>
         {
             var userIdentityMovil = new DummyUserIdentityMovil("DRCD");
             _fichaSupervisionEmpleoRequest = getFichaSupervisionEmpleoRequest();
+
+            _dummyMovilRequest = new MovilRequest()
+            {
+                token = "token",
+                data = "data"
+            };
+            var tokenizer = Mock.Of<ITokenizer>();
+            Mock.Get(tokenizer)
+                .Setup(x => x.Detokenize(Moq.It.IsAny<string>(), Moq.It.IsAny<NancyContext>()))
+                .Returns(userIdentityMovil);
+            var serializerJson = Mock.Of<ISerializeJsonRequest>();
+            var encryptRequestProvider = getEncrypter();
+
 
             var command = Mock.Of<ICommandInsertFichaDeSupervision>();
             var fileGetter = Mock.Of<IFileGetter>();
@@ -46,15 +63,22 @@ namespace CNISS_Tests.Enterprise_Test.Entities_Test.Empleo_Test.Module
                     x =>
                         x.existsFile(@"/ImagenesMoviles",
                             _fichaSupervisionEmpleoRequest.fotografiaBeneficiario.ToString(), ".jpeg")).Returns(false);
-
+            Mock.Get(serializerJson)
+              .Setup(x => x.fromJson<FichaSupervisionEmpleoRequest>(Moq.It.IsAny<string>()))
+              .Returns(_fichaSupervisionEmpleoRequest);
             _browser = new Browser(x =>
             {
                 x.Module<FichaEmpleoSupervisionModuleInsert>();
-                x.Dependencies(command, fileGetter);
-                x.RequestStartup((container, pipelines, context) =>
-                {
-                    context.CurrentUser = userIdentityMovil;
-                });
+                x.MappedDependencies(new[]
+                                     {
+                                         new Tuple<Type, object>(typeof(ISerializeJsonRequest),serializerJson),
+                                         new Tuple<Type, object>(typeof(Func<string,IEncrytRequestProvider>),encryptRequestProvider),
+                                        new Tuple<Type, object>(typeof(ITokenizer),tokenizer),
+                                        new Tuple<Type, object>(typeof(ICommandInsertFichaDeSupervision),command),
+                                        new Tuple<Type, object>(typeof(IFileGetter),fileGetter)
+                                       
+                                     });
+                
             });
 
 
@@ -67,6 +91,11 @@ namespace CNISS_Tests.Enterprise_Test.Entities_Test.Empleo_Test.Module
 
          It should_return_error = () => _browserResponse.StatusCode.ShouldEqual(HttpStatusCode.BadRequest);
 
+         private static Func<string, IEncrytRequestProvider> getEncrypter()
+         {
+             var x = new Func<string, IEncrytRequestProvider>(z => new DummyEncrytRequestProvider());
+             return x;
+         }
 
         private static FichaSupervisionEmpleoRequest getFichaSupervisionEmpleoRequest()
         {
